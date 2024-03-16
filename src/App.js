ba228a7e281aca2dd25ab02c0602085ec48df4f4
Dhaarx <dharineesh.nm2022ai-ds@sece.ac.in>
2024-03-16 19:05:48 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Home from './screen/home';
 
 function App() {
   return (
-  <Home/>
+    <>
+    <Home />
+    </>
   );
 }
 
--- src/screen/home.js
+++ src/screen/home.js
@@ -1,15 +1,33 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
- const Home=()=>{
+import Navbar from '../navbar/navbar';
+import "./home.css";
+import ima from '../images/farm.jpg' ;
+const Home=()=>{
     return(
-        <div>
-            <h1>It is a home page</h1>
-            <br/>
-            <center>
-            <button style={{ borderRadius: '10%' }}>
-            <Link to="/store">Store</Link>
-            </button>
-            </center>
+        <div className="back" >
+            <Navbar />
+            
+                <div className="firstline">
+                  <p >Are you in NEED?</p>
+                </div>
+                <br/>
+                  <br/>
+                <div className="p1">
+                    <p >Don’t Wait!</p>
+                </div>
+                <br/>
+                <br/>
+                <br/>
+                  <br/>
+                <div className="p2">
+                    <p >Revamp. Restock. Recharge.</p>
+                </div>
+                <div>
+                    <button className='btn1'>Check Here!</button>
+                </div>
+               <div className='ima'>
+                <img src={ima} alt='imag'></img>
+               </div>
         </div>
     )
  }
