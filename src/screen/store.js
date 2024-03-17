07d2839179022a8f944857b3f878c7024981a67e
Dhaarx <dharineesh.nm2022ai-ds@sece.ac.in>
2024-03-17 12:05:27 +0530

    hjk

--- src/screen/store.js
+++ src/screen/store.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './store.css';
-import { FaInstagram } from "react-icons/fa";
-import { FaSquareXTwitter } from "react-icons/fa6";
-import { FaFacebookSquare } from "react-icons/fa";
+
 import { FaCartPlus } from "react-icons/fa";
  const Store=()=>{
     return(
